0920901de24854d408fa3147597846c7e3875c11
Thiwanka Madushan <76912747+Thiwanka570@users.noreply.github.com>
2023-11-04 11:55:17 +0530

    Signed-off-by: Thiwanka Madushan <76912747+Thiwanka570@users.noreply.github.com>

---
+++ spring_backend/src/main/java/com/crudaplication/spring_backend/SpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crudaplication.spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendApplication.class, args);
+	}
+
+}
---
+++ spring_backend/src/test/java/com/crudaplication/spring_backend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudaplication.spring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
